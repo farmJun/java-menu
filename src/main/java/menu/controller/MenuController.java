752abd180e8a8378582e20f393e7e47ae34d8377
박준영 <wnsdud6969@naver.com>
2023-12-15 19:17:49 +0900

    feat(MenuController): 점심 메뉴 추천 시작을 알린다

---
+++ src/main/java/menu/controller/MenuController.java
@@ -0,0 +1,10 @@
+package menu.controller;
+
+import menu.view.OutputView;
+
+public class MenuController {
+
+    private void printStartRecommendation() {
+        OutputView.printStartRecommendation();
+    }
+}
