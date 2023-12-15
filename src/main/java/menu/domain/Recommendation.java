c475e3f2a7eb45c4ac0921146504b98c7c00a1f6
박준영 <wnsdud6969@naver.com>
2023-12-15 19:39:17 +0900

    refactor(Recommendation): 하드 코딩된 값을 상수화하다

--- src/main/java/menu/domain/Recommendation.java
+++ src/main/java/menu/domain/Recommendation.java
@@ -5,18 +5,26 @@ import java.util.List;
 
 public class Recommendation {
 
+    private static final int MINIMUM_CATEGORY_CODE = 1;
+    private static final int MAXIMUM_CATEGORY_CODE = 5;
+    private static final int FIRST = 0;
+
     private Recommendation() {
 
     }
 
     public static Category recommendCategory() {
-        Category category = Category.findByCode(Randoms.pickNumberInRange(1, 5));
+        Category category = Category.findByCode(generateRandomCode());
         return category;
     }
 
     public static Menu recommendMenu(Category category) {
         List<String> menuNames = category.getMenuNames();
-        String menu = Randoms.shuffle(menuNames).get(0);
+        String menu = Randoms.shuffle(menuNames).get(FIRST);
         return Menu.find(menu);
+    }
+
+    private static int generateRandomCode() {
+        return Randoms.pickNumberInRange(MINIMUM_CATEGORY_CODE, MAXIMUM_CATEGORY_CODE);
     }
 }
