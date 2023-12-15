c06a05e3276955a82f54de454e12eb9a025762da
박준영 <wnsdud6969@naver.com>
2023-12-15 19:12:03 +0900

    feat(Recommendation): 카테고리에 맞는 메뉴를 추천한다

--- src/main/java/menu/domain/Recommendation.java
+++ src/main/java/menu/domain/Recommendation.java
@@ -1,6 +1,7 @@
 package menu.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
 
 public class Recommendation {
 
@@ -11,5 +12,11 @@ public class Recommendation {
     public static Category recommendCategory() {
         Category category = Category.findByCode(Randoms.pickNumberInRange(1, 5));
         return category;
+    }
+
+    public static Menu recommendMenu(Category category) {
+        List<String> menuNames = category.getMenuNames();
+        String menu = Randoms.shuffle(menuNames).get(0);
+        return Menu.find(menu);
     }
 }
