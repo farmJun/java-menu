612e94dcfc9333cf0e82863f7ed6f95483a09556
박준영 <wnsdud6969@naver.com>
2023-12-15 19:46:45 +0900

    refactor(OutputView): 하드 코딩된 값을 상수화하다

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -8,51 +8,66 @@ import menu.domain.Day;
 
 public class OutputView {
 
+    private static final String START_MENU_RECOMMENDATION = "점심 메뉴 추천을 시작합니다.\n";
+    private static final String RESULT_OF_RECOMMENDATION = "메뉴 추천 결과입니다.";
+    private static final String END_OF_RECOMMENDATION = "추천을 완료했습니다.";
+    private static final String DELIMITER = " | ";
+    private static final String PREFIX = "[ ";
+    private static final String SUFFIX = " ]";
+    private static final String CLASSIFICATION = "구분";
+    private static final String CATEGORY = "카테고리";
+
+
     private OutputView() {
 
     }
 
     public static void printStartRecommendation() {
-        System.out.println("점심 메뉴 추천을 시작합니다.\n");
+        System.out.println(START_MENU_RECOMMENDATION);
     }
 
     public static void printResult() {
-        System.out.println("메뉴 추천 결과입니다.");
+        System.out.println(RESULT_OF_RECOMMENDATION);
     }
 
     public static void printDays() {
-        StringJoiner headerJoiner = new StringJoiner(" | ");
-        headerJoiner.add("구분");
+        StringJoiner headerJoiner = new StringJoiner(DELIMITER);
+        headerJoiner.add(CLASSIFICATION);
+
         for (int i = 0; i < 5; i++) {
             Day day = Day.find(i);
             headerJoiner.add(day.getDayName());
         }
-        System.out.println("[ " + headerJoiner + " ]");
+
+        System.out.println(PREFIX + headerJoiner + SUFFIX);
     }
 
     public static void printCategories(List<Category> categories) {
-        StringJoiner categoryJoiner = new StringJoiner(" | ");
-        categoryJoiner.add("카테고리");
+        StringJoiner categoryJoiner = new StringJoiner(DELIMITER);
+        categoryJoiner.add(CATEGORY);
+
         for (Category category : categories) {
             categoryJoiner.add(category.getName());
         }
-        System.out.println("[ " + categoryJoiner + " ]");
+
+        System.out.println(PREFIX + categoryJoiner + SUFFIX);
     }
 
     public static void printCoachAndMenus(List<Coach> coaches) {
         for (Coach coach : coaches) {
-            StringJoiner coachJoiner = new StringJoiner(" | ");
+            StringJoiner coachJoiner = new StringJoiner(DELIMITER);
             coachJoiner.add(coach.getName());
-            List<String> recom = coach.getRecommendedMenus();
+            List<String> recommendedMenus = coach.getRecommendedMenus();
 
-            for (String menu : recom) {
+            for (String menu : recommendedMenus) {
                 coachJoiner.add(menu);
             }
-            System.out.println("[ " + coachJoiner + " ]");
+
+            System.out.println(PREFIX + coachJoiner + SUFFIX);
         }
     }
 
     public static void printEndRecommendation() {
-        System.out.println("추천을 완료했습니다.");
+        System.out.println(END_OF_RECOMMENDATION);
     }
 }
