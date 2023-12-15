494111b3676cb1b236cd56c384d454a41e6ab933
박준영 <wnsdud6969@naver.com>
2023-12-15 19:13:41 +0900

    feat(OutputView): 요일을 출력한다

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,5 +1,8 @@
 package menu.view;
 
+import java.util.StringJoiner;
+import menu.domain.Day;
+
 public class OutputView {
 
     private OutputView() {
@@ -9,4 +12,15 @@ public class OutputView {
     public static void printStartRecommendation() {
         System.out.println("점심 메뉴 추천을 시작합니다.\n");
     }
+
+    public static void printDays() {
+        StringJoiner headerJoiner = new StringJoiner(" | ");
+        headerJoiner.add("구분");
+        for (int i = 0; i < 5; i++) {
+            Day day = Day.find(i);
+            headerJoiner.add(day.getDayName());
+        }
+        System.out.println("[ " + headerJoiner + " ]");
+    }
+
 }
