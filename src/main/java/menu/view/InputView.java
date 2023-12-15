aab85ef91c57b93346977202f281b2f28d8a6b48
박준영 <wnsdud6969@naver.com>
2023-12-15 19:04:13 +0900

    feat(InputView): 먹지 못하는 메뉴를 입력 받는 기능을 구현하다

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -3,6 +3,7 @@ package menu.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
+import menu.domain.Name;
 
 public class InputView {
 
@@ -12,6 +13,12 @@ public class InputView {
 
     public static List<String> readNames() {
         System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+        String input = Console.readLine();
+        return Arrays.asList(input.split(","));
+    }
+
+    public static List<String> readInedibleMenus(Name name) {
+        System.out.println(String.format("%s(이)가 못 먹는 메뉴를 입력해 주세요.", name.getName()));
         String input = Console.readLine();
         return Arrays.asList(input.split(","));
     }
