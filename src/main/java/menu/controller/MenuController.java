570bd3f456e5048af96cbbac3817743426f6465b
박준영 <wnsdud6969@naver.com>
2023-12-15 19:22:32 +0900

    feat(MenuController): 코치들을 생성한다

--- src/main/java/menu/controller/MenuController.java
+++ src/main/java/menu/controller/MenuController.java
@@ -1,7 +1,10 @@
 package menu.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import menu.domain.Coach;
+import menu.domain.Coaches;
 import menu.domain.Menu;
 import menu.domain.Menus;
 import menu.domain.Name;
@@ -26,6 +29,17 @@ public class MenuController {
                 System.out.println(e.getMessage());
             }
         }
+    }
+
+    private Coaches getCoaches(Names names) {
+        List<Coach> coaches = new ArrayList<>();
+
+        for (Name name : names.getNames()) {
+            Menus inedibleMenus = readInedibleMenus(name);
+            Coach coach = new Coach(name, inedibleMenus);
+            coaches.add(coach);
+        }
+        return new Coaches(coaches);
     }
 
     private Menus readInedibleMenus(Name name) {
