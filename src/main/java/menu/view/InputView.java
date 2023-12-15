a3d1f794de917f4a518e79e86782e80a5c3f33c9
박준영 <wnsdud6969@naver.com>
2023-12-15 19:41:06 +0900

    refactor(InputView): 하드 코딩된 값을 상수화하다

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -7,19 +7,23 @@ import menu.domain.Name;
 
 public class InputView {
 
+    private static final String REGEX = ",";
+    private static final String INPUT_COACH_NAME = "코치의 이름을 입력해 주세요. (, 로 구분)";
+    private static final String INPUT_INEDIBLE_MENU = "%s(이)가 못 먹는 메뉴를 입력해 주세요.";
+
     private InputView() {
 
     }
 
     public static List<String> readNames() {
-        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+        System.out.println(INPUT_COACH_NAME);
         String input = Console.readLine();
-        return Arrays.asList(input.split(","));
+        return Arrays.asList(input.split(REGEX));
     }
 
     public static List<String> readInedibleMenus(Name name) {
-        System.out.println(String.format("%s(이)가 못 먹는 메뉴를 입력해 주세요.", name.getName()));
+        System.out.println(String.format(INPUT_INEDIBLE_MENU, name.getName()));
         String input = Console.readLine();
-        return Arrays.asList(input.split(","));
+        return Arrays.asList(input.split(REGEX));
     }
 }
