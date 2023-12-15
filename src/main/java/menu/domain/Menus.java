9e11037847dee37005557963bb07f3bb3ad3dc3d
박준영 <wnsdud6969@naver.com>
2023-12-15 19:29:58 +0900

    refactor(Menus): 하드 코딩된 값을 상수화하다

--- src/main/java/menu/domain/Menus.java
+++ src/main/java/menu/domain/Menus.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 public class Menus {
 
+    private static final String REPETITION_MENUS = "[ERROR] 메뉴가 중복되었습니다.";
     private final List<Menu> menus;
 
     public Menus(List<Menu> menus) {
@@ -32,7 +33,7 @@ public class Menus {
 
     private void vaildate(List<Menu> menus) {
         if (menus.size() != menus.stream().distinct().count()) {
-            throw new IllegalArgumentException("[ERROR] 중복 이름");
+            throw new IllegalArgumentException(REPETITION_MENUS);
         }
     }
 }
