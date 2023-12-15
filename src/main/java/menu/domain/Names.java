d036c294104516605257332aa572d4e05c56008e
박준영 <wnsdud6969@naver.com>
2023-12-15 19:37:03 +0900

    refactor(Names): 하드 코딩된 값을 상수화하다

--- src/main/java/menu/domain/Names.java
+++ src/main/java/menu/domain/Names.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 public class Names {
 
+    private static final int MINIMUM_NAME_LENGTH = 2;
+    private static final int MAXIMUM_NAME_LENGTH = 5;
+    private static final String LESS_THAN_MINIMUM_COACH = "[ERROR] 코치는 최소 2명 이상 입력해야 합니다.";
+    private static final String OVER_MAXIMUM_COACH = "[ERROR] 코치는 최대 5명까지 입력 가능합니다.";
+    private static final String REPETITION_NAME = "[ERROR] 코치 이름이 중복되서는 안됩니다.";
+
     private final List<Name> names;
 
     public Names(List<Name> names) {
@@ -16,12 +22,16 @@ public class Names {
     }
 
     private void vaildate(List<Name> names) {
-        if (names.size() < 2 || names.size() > 5) {
-            throw new IllegalArgumentException("[ERROR] 너무 많은 코치");
+        if (names.size() < MINIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException(LESS_THAN_MINIMUM_COACH);
+        }
+
+        if (names.size() > MAXIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException(OVER_MAXIMUM_COACH);
         }
 
         if (names.size() != names.stream().distinct().count()) {
-            throw new IllegalArgumentException("[ERROR] 중복 이름");
+            throw new IllegalArgumentException(REPETITION_NAME);
         }
     }
 }
