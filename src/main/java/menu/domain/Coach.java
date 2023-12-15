917740a9b12da7b5cc8c71c2be5ee5848acb53e2
박준영 <wnsdud6969@naver.com>
2023-12-15 19:05:43 +0900

    feat(Coach): 먹지 못하는 메뉴의 개수를 검증하다

---
+++ src/main/java/menu/domain/Coach.java
@@ -0,0 +1,26 @@
+package menu.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Coach {
+
+    private final Name name;
+    private final Menus inedibleMenus;
+    private final Menus recommendedMenus;
+
+
+    public Coach(Name name, Menus inedibleMenus) {
+        validate(inedibleMenus);
+        this.name = name;
+        this.inedibleMenus = inedibleMenus;
+        this.recommendedMenus = new Menus(new ArrayList<>());
+    }
+
+    private void validate(Menus inedibleMenus) {
+        if (inedibleMenus.size() > 2) {
+            throw new IllegalArgumentException("[ERROR] 편식 쟁이");
+        }
+    }
+
+}
