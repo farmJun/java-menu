d8b7ef1008f6e79427f5c8fc51e2ba9d2fcc407c
박준영 <wnsdud6969@naver.com>
2023-12-15 19:33:32 +0900

    refactor(Name): 하드 코딩된 값을 상수화하다

--- src/main/java/menu/domain/Name.java
+++ src/main/java/menu/domain/Name.java
@@ -2,6 +2,11 @@ package menu.domain;
 
 public class Name {
 
+    private static final int MINIMUM_NAME_LENGTH = 2;
+    private static final int MAXIMUM_NAME_LENGTH = 4;
+    private static final String LESS_THAN_MINIMUM_NAME_LENGTH = "[ERROR] 최소 길이 미만의 이름입니다.";
+    private static final String OVER_MAXIMUM_NAME_LENGTH = "[ERROR] 최대 길이 초과의 이름입니다.";
+
     private final String name;
 
     public Name(String name) {
@@ -14,8 +19,12 @@ public class Name {
     }
 
     private void validate(String name) {
-        if (name.length() < 2 || name.length() > 4) {
-            throw new IllegalArgumentException("[ERROR] 이름 길이");
+        if (name.length() < MINIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException(LESS_THAN_MINIMUM_NAME_LENGTH);
+        }
+
+        if (name.length() > MAXIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException(OVER_MAXIMUM_NAME_LENGTH);
         }
     }
 }
