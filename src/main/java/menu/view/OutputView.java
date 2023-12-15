7264caa30e64f8ffa45288b22d0353256843cf0a
박준영 <wnsdud6969@naver.com>
2023-12-15 19:13:01 +0900

    feat(OutputView): 점심 메뉴 추천 시작을 알린다

---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,12 @@
+package menu.view;
+
+public class OutputView {
+
+    private OutputView() {
+
+    }
+
+    public static void printStartRecommendation() {
+        System.out.println("점심 메뉴 추천을 시작합니다.\n");
+    }
+}
